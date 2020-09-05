72e6f271ee7288b32ea2e439329319780c0cafa7
Soumya Ranjan Swain <srswain2@gmail.com>
2020-09-06 02:25:37 +0530

    Card Layout UX Design

--- CardView.tsx
+++ CardView.tsx
@@ -1,24 +1,90 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
+    Image,
     Text,
     View,
-    StyleSheet, ScrollView
+    StyleSheet,
+    ScrollView,
+    Animated, TouchableOpacity
 } from 'react-native';
-import { SegmentedControls } from 'react-native-radio-buttons';
-import Swiper from 'react-native-swiper';
+import {SegmentedControls} from 'react-native-radio-buttons';
+import { Card, ListItem, Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default class CardView extends Component<{}, { selectedOption: string, tabIndex: number }> {
+
+const DEFAULT_TAB_INDEX = 1;
+const DEFAULT_TAB_TITLE = "On Going";
+
+export default class CardView extends Component<{}, {
+    selectedOption: string,
+    tabIndex: number,
+    tabControlStartValue: any,
+    tabControlEndValue: any,
+    tabControlDuration: any
+}> {
     Past = () => (
-        <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
+        <View style={[styles.scene, {backgroundColor: '#ff4081'}]}/>
     );
 
     OnGoing = () => (
-        <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
+        <View style={[styles.scene, {backgroundColor: '#673ab7'}]}/>
     );
 
     ComingUp = () => (
-        <View style={[styles.scene, { backgroundColor: '#ff3bb7' }]} />
+        <View style={[styles.scene, {backgroundColor: '#ff3bb7'}]}/>
     );
+
+    data = [
+        {
+            platform: "codechef",
+            platformName: "Codechef",
+            title: "September Challenge 2020",
+            dateFrom: "04 Sep 2020 15:00:00",
+            dateTo: "14 Sep 2020 15:00:00",
+            logo: "./assets/platforms/small/logo-small-1.png",
+            link: "https://www.codechef.com/SEPT20",
+            backgroundColor: "#C1E4E5"
+        },
+        {
+            platform: "codechef",
+            platformName: "Codechef",
+            title: "September Cook-Off 2020",
+            dateFrom: "20 Sep 2020 21:30:00",
+            dateTo: "21 Sep 2020 00:00:00",
+            logo: "./assets/platforms/small/logo-small-1.png",
+            link: "https://www.codechef.com/COOK122",
+            backgroundColor: "#D3EABA"
+        },
+        {
+            platform: "codechef",
+            platformName: "Codechef",
+            title: "September Cook-Off 2020",
+            dateFrom: "20 Sep 2020 21:30:00",
+            dateTo: "21 Sep 2020 00:00:00",
+            logo: "./assets/platforms/small/logo-small-1.png",
+            link: "https://www.codechef.com/COOK122",
+            backgroundColor: "#FBF0D6"
+        },
+        {
+            platform: "codechef",
+            platformName: "Codechef",
+            title: "September Cook-Off 2020",
+            dateFrom: "20 Sep 2020 21:30:00",
+            dateTo: "21 Sep 2020 00:00:00",
+            logo: "./assets/platforms/small/logo-small-1.png",
+            link: "https://www.codechef.com/COOK122",
+            backgroundColor: "#FDE2C6"
+        },
+        {
+            platform: "codechef",
+            platformName: "Codechef",
+            title: "September Cook-Off 2020",
+            dateFrom: "20 Sep 2020 21:30:00",
+            dateTo: "21 Sep 2020 00:00:00",
+            logo: "./assets/platforms/small/logo-small-1.png",
+            link: "https://www.codechef.com/COOK122",
+            backgroundColor: "#D3EEEF"
+        }];
 
     options = [
         "Past",
@@ -28,21 +94,119 @@ export default class CardView extends Component<{}, { selectedOption: string, ta
 
     constructor(props) {
         super(props);
-        this.state = {selectedOption: 'On Going', tabIndex: 1};
+        this.state = {
+            selectedOption: DEFAULT_TAB_TITLE,
+            tabIndex: DEFAULT_TAB_INDEX,
+            tabControlStartValue: Array(3).fill(new Animated.Value(0)),
+            tabControlEndValue: Array(3).fill(1),
+            tabControlDuration: 500,
+        };
     }
 
-    setSelectedOption(selectedOption){
-        let idx1 = this.options.findIndex((element) => selectedOption === element);
-        let idx2 = this.options.findIndex((element) => this.state.selectedOption === element)
-        this.refs.swiper.scrollBy(idx1 - idx2, true);
+    componentDidMount() {
+        // Animated.timing(this.state.tabControlStartValue[DEFAULT_TAB_INDEX], {
+        //     toValue: this.state.tabControlEndValue[DEFAULT_TAB_INDEX],
+        //     duration: this.state.tabControlDuration,
+        //     useNativeDriver: true,
+        // }).start();
+        let updatedTabControlStartValue = this.state.tabControlStartValue;
+        updatedTabControlStartValue[this.state.tabIndex] = new Animated.Value(1);
+
         this.setState({
-            selectedOption,
-            tabIndex: this.options.findIndex((element) => selectedOption === element)
+            tabControlStartValue: updatedTabControlStartValue
         });
     }
 
-    scrollBy() {
+    setSelectedOption(selectedOption) {
+        let currentTabIndex = this.options.findIndex((element) => selectedOption === element);
+        if (currentTabIndex === this.state.tabIndex) {
+            return;
+        }
+        Animated.timing(this.state.tabControlStartValue[currentTabIndex], {
+            toValue: this.state.tabControlEndValue[currentTabIndex],
+            duration: this.state.tabControlDuration,
+            useNativeDriver: true,
+        }).start();
 
+        let updatedTabControlStartValue = this.state.tabControlStartValue// Array(3).fill(new Animated.Value(1))
+        // updatedTabControlStartValue[currentTabIndex] = new Animated.Value(1);
+        updatedTabControlStartValue[this.state.tabIndex] = new Animated.Value(0);
+
+        this.setState({
+            selectedOption,
+            tabIndex: currentTabIndex,
+            tabControlStartValue: updatedTabControlStartValue
+        });
+    }
+
+    loadCard(cardObject) {
+        return (
+            <Card containerStyle={{backgroundColor: cardObject.backgroundColor, borderColor: cardObject.backgroundColor, margin: 10, marginBottom: 8, paddingLeft: 0, paddingRight: 0, borderRadius: 5, elevation: 10, shadowColor: '#000',
+                shadowOffset: {width: 0, height: 5},
+                shadowOpacity: 0.3,
+                shadowRadius: 7}}>
+                <View style={{flexDirection:'row', marginLeft: 0}}>
+                    <View style={{padding: 0, margin: 0, borderRadius: 9, alignItems: "center", alignContent: "center"}}>
+                        <Image
+                            style={{ width: 120, height: 120, margin: 0, marginLeft: 15, borderRadius: 15, alignItems: "center", alignContent: "center"}}
+                            source={require('./assets/platforms/small/ls1.png')}
+                        />
+                    </View>
+                    <View style={{flex: 1, padding: 0, margin: 0}}>
+                        <Text style={{fontSize: 24, fontWeight: "bold", textAlign: 'center'}}>{cardObject.title}</Text>
+                        <View style={{ flexDirection:'row'}}>
+                            <View style={{flex: 1}}>
+                                <Text style={{color: "#555", fontSize: 18, fontFamily: "sans-serif-light", flex: 1, textAlign: 'center', textAlignVertical: "center"}}>START:</Text>
+                                <View style={{flex: 1}}>
+                                    <Text style={{color: "#000", fontSize: 15, fontFamily: "sans-serif-light", fontWeight: "bold", flex: 1, alignSelf: "stretch", textAlign: 'center'}}>4th Sep, 2020</Text>
+                                    <Text style={{color: "#000", fontSize: 15, fontFamily: "sans-serif-light", fontWeight: "bold", flex: 1, alignSelf: "stretch", textAlign: 'center'}}>15:00</Text>
+                                </View>
+                            </View>
+                            <View style={{flex: 1}}>
+                                <Text style={{color: "#555", fontSize: 18, fontFamily: "sans-serif-light", flex: 1,textAlign: 'center', textAlignVertical: "center"}}>END:</Text>
+                                <View>
+                                    <Text style={{color: "#000", fontSize: 15, fontFamily: "sans-serif-light", fontWeight: "bold", flex: 1, alignSelf: "stretch", textAlign: 'center'}}>14th Sep, 2020</Text>
+                                    <Text style={{color: "#000", fontSize: 15, fontFamily: "sans-serif-light", fontWeight: "bold", flex: 1, alignSelf: "stretch", textAlign: 'center'}}>21:30</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={{ flexDirection:'row', marginTop: 10}}>
+                            <View style={{flex: 1, marginLeft: 20, marginRight: 10}}>
+                                <Button
+                                    buttonStyle={{backgroundColor: cardObject.backgroundColor}}
+                                    titleStyle={{color: "#26474E", fontFamily: "sans-serif-light"}}
+                                    icon={
+                                        <Icon
+                                            name="external-link"
+                                            size={15}
+                                            color="#26474E"
+                                            style={{marginRight: 8}}
+                                        />
+                                    }
+                                    title="Link"
+                                    raised={true}
+                                />
+                            </View>
+                            <View style={{flex: 1, marginLeft: 10, marginRight: 20}}>
+                                <Button
+                                    buttonStyle={{backgroundColor: cardObject.backgroundColor}}
+                                    titleStyle={{color: "#26474E", fontFamily: "sans-serif-light"}}
+                                    icon={
+                                        <Icon
+                                            name="bell"
+                                            size={15}
+                                            color="#26474E"
+                                            style={{marginRight: 10}}
+                                        />
+                                    }
+                                    title="Reminder"
+                                    raised={true}
+                                />
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </Card>);
     }
 
     render() {
@@ -50,72 +214,57 @@ export default class CardView extends Component<{}, { selectedOption: string, ta
             <View style={styles.container}>
                 <SegmentedControls
                     tint={'#26474E'}
-                    selectedTint= {'white'}
-                    backTint= {'#75a0a5'}
-                    containerBorderTint= {'#75a0a5'}
-                    options={ this.options }
-                    allowFontScaling={ false } // default: true
-                    onSelection={ this.setSelectedOption.bind(this) }
-                    selectedOption={ this.state.selectedOption }
+                    selectedTint={'white'}
+                    backTint={'#75a0a5'}
+                    containerBorderTint={'#75a0a5'}
+                    options={this.options}
+                    allowFontScaling={false} // default: true
+                    onSelection={this.setSelectedOption.bind(this)}
+                    selectedOption={this.state.selectedOption}
                     optionStyle={{fontFamily: 'monospace'}}
                     optionContainerStyle={{flex: 1}}
                 />
-                <Swiper
-                    style={styles.wrapper}
-                    showsButtons={false}
-                    showsPagination={false}
-                    index={1}
-                    onIndexChanged = {(index) => {this.setState({selectedOption: this.options[index]})}}
-                    ref='swiper'>
-                    <ScrollView style={styles.slide1}>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                            <Text style={styles.text}>Beautiful</Text>
-                    </ScrollView>
-                    <View style={styles.slide2}>
-                        <Text style={styles.text}>Beautiful</Text>
-                    </View>
-                    <View style={styles.slide3}>
-                        <Text style={styles.text}>And simple</Text>
-                    </View>
-                </Swiper>
-                {/*<CardFlip style={styles.cardContainer} ref={card => (this.card = card)}>*/}
-                {/*    <TouchableOpacity*/}
-                {/*        activeOpacity={1}*/}
-                {/*        style={[styles.card, styles.card1]}*/}
-                {/*        onPress={() => this.card.flip()}>*/}
-                {/*        <Text style={styles.label}>AB</Text>*/}
-                {/*    </TouchableOpacity>*/}
-                {/*    <TouchableOpacity*/}
-                {/*        activeOpacity={1}*/}
-                {/*        style={[styles.card, styles.card2]}*/}
-                {/*        onPress={() => this.card.flip()}>*/}
-                {/*        <Text style={styles.label}>CD</Text>*/}
-                {/*    </TouchableOpacity>*/}
-                {/*</CardFlip>*/}
+                {this.state.tabIndex === 0 ?
+                    <Animated.View style={[styles.animatedViewSlide, {opacity: this.state.tabControlStartValue[0]}]}>
+                        <ScrollView style={styles.slide1} contentContainerStyle={{
+                            flexGrow: 1
+                        }}>
+                            {this.data.map((contest) => {
+                                return this.loadCard(contest);
+                            })}
+
+                        </ScrollView>
+                    </Animated.View>
+                    : null
+                }
+                {this.state.tabIndex === 1 ?
+                    <Animated.View style={[styles.animatedViewSlide, {opacity: this.state.tabControlStartValue[1]}]}>
+                        <ScrollView style={styles.slide2} contentContainerStyle={{
+                            flexGrow: 1,
+                            backgroundColor: "#FFF"
+                        }}>
+                            {this.data.map((contest) => {
+                                return this.loadCard(contest);
+                            })}
+
+                        </ScrollView>
+                    </Animated.View>
+                    : null
+                }
+                {this.state.tabIndex === 2 ?
+                    <Animated.View style={[styles.animatedViewSlide, {opacity: this.state.tabControlStartValue[2]}]}>
+                        <ScrollView style={styles.slide3} contentContainerStyle={{
+                            flexGrow: 1,
+                            backgroundColor: "#FFF"
+                        }}>
+                            {this.data.map((contest) => {
+                                return this.loadCard(contest);
+                            })}
+
+                        </ScrollView>
+                    </Animated.View>
+                    : null
+                }
             </View>
 
         );
@@ -125,12 +274,13 @@ export default class CardView extends Component<{}, { selectedOption: string, ta
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#f1f5f6'
+        backgroundColor: '#fff'
     },
     cardContainer: {
         flexDirection: 'row',
         height: 170,
         margin: 12,
+        elevation: 2
     },
     card: {
         flexDirection: 'row',
@@ -144,7 +294,7 @@ const styles = StyleSheet.create({
             height: 5,
         },
         shadowOpacity: 0.5,
-        elevation: 10
+        elevation: 2
     },
     card1: {
         backgroundColor: '#9ebcc0',
@@ -155,30 +305,26 @@ const styles = StyleSheet.create({
     label: {
         fontSize: 20
     },
-    scene: {
-
-    },
-    wrapper: {
-    },
+    scene: {},
+    wrapper: {},
     slide1: {
         flex: 1,
-        backgroundColor: '#9DD6EB'
+        backgroundColor: '#fff'
     },
     slide2: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#97CAE5'
+        backgroundColor: '#fff'
     },
     slide3: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#92BBD9'
+        backgroundColor: '#fff'
     },
     text: {
-        color: '#fff',
+        color: '#000',
         fontSize: 30,
         fontWeight: 'bold'
+    },
+    animatedViewSlide: {
+        flex: 1,
     }
 });
